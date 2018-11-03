afe862bf13656b2e5b9a4f88f78dedcde68ebff0
Szymon Harabasz <szymon.harabasz@gmail.com>
2018-11-03 19:01:39 +0100

    Solved up to Exercise 3.29, 3.24 still missing.

--- src/main/scala/fpinscala/datastrcutures/MyTree.scala
+++ src/main/scala/fpinscala/datastrcutures/MyTree.scala
@@ -19,4 +19,20 @@ object MyTree {
     case Leaf(_) => 1
     case Branch(l, r) => 1 + (depth(l) max depth(r))
   }
+
+  def map[A,B](as: MyTree[A])(f: A => B): MyTree[B] = as match {
+    case Leaf(a) => Leaf(f(a))
+    case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+  }
+
+  def fold[A,B](as: MyTree[A])(f: A => B, g: (B, B) => B): B = as match {
+    case Leaf(a) => f(a)
+    case Branch(l, r) => g(fold(l)(f,g), fold(r)(f,g))
+  }
+
+  def sizeFold[A](as: MyTree[A]): Int = fold(as)(_ => 1, (a: Int, b:Int) => a + b)
+  def maxFold(ns: MyTree[Int]): Int = fold(ns)((a: Int) => a, (a: Int, b: Int) => a max b)
+  def depthFold(as: MyTree[Int]): Int = fold(as)(_ => 1, (a: Int, b: Int) => 1 + (a max b))
+  def mapFold[A,B](as: MyTree[A])(f: A => B): MyTree[B] =
+    fold(as)(a => Leaf(f(a)), (a: MyTree[B], b: MyTree[B]) => Branch(a,b))
 }
--- src/test/scala/fpinscala/datastrcutures/MyTreeTest.scala
+++ src/test/scala/fpinscala/datastrcutures/MyTreeTest.scala
@@ -14,4 +14,20 @@ class MyTreeTest extends FunSuite {
     assert(MyTree.depth(Branch(Branch(Leaf(3), Leaf(4)), Leaf(3))) == 3)
     assert(MyTree.depth(Branch(Leaf(6), Branch(Leaf(5), Branch(Leaf(3), Leaf(4))))) == 4)
   }
+  test("-> Exercise 3.28") {
+    assert(MyTree.map(Branch(Branch(Leaf(3), Leaf(4)), Leaf(3)))(x => x*x) ==
+      Branch(Branch(Leaf(9), Leaf(16)), Leaf(9)))
+  }
+  test("-> Exercise 3.29") {
+    assert(MyTree.sizeFold(Branch(Branch(Leaf(3), Leaf(2)), Leaf(4))) ==
+      MyTree.size(Branch(Branch(Leaf(3), Leaf(2)), Leaf(4))))
+    assert(MyTree.maxFold(Branch(Branch(Leaf(3), Leaf(4)), Leaf(3))) ==
+      MyTree.max(Branch(Branch(Leaf(3), Leaf(4)), Leaf(3))))
+    assert(MyTree.depthFold(Branch(Branch(Leaf(3), Leaf(4)), Leaf(3))) ==
+      MyTree.depth(Branch(Branch(Leaf(3), Leaf(4)), Leaf(3))))
+    assert(MyTree.mapFold(Branch(Branch(Leaf(3), Leaf(4)), Leaf(3)))(x => x*x) ==
+      MyTree.map(Branch(Branch(Leaf(3), Leaf(4)), Leaf(3)))(x => x*x))
+
+  }
+
 }
