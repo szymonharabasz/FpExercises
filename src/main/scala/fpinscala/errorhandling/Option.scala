457ffd3357d23fb0ae22e7da1f983a75b1a21027
Szymon Harabasz <szymon.harabasz@gmail.com>
2018-11-06 00:08:30 +0100

    map and filter expressed in terms of flatMap

--- src/main/scala/fpinscala/errorhandling/Option.scala
+++ src/main/scala/fpinscala/errorhandling/Option.scala
@@ -1,10 +1,8 @@
 package fpinscala.errorhandling
 
 sealed trait Option[+A] {
-  def map[B](f: A => B): Option[B] = this match {
-    case None => None
-    case Some(a) => Some(f(a))
-  }
+
+  def map[B](f: A => B): Option[B] = flatMap(a => Some(f(a)))
   def flatMap[B](f: A => Option[B]): Option[B] = this match {
     case None => None
     case Some(a) => f(a)
@@ -17,10 +15,7 @@ sealed trait Option[+A] {
     case None => ob
     case a => a
   }
-  def filter(f: A => Boolean): Option[A] = this match {
-    case None => None
-    case Some(a) => if (f(a)) Some(a) else None
-  }
+  def filter(f: A => Boolean): Option[A] = flatMap(a => if (f(a)) Some(a) else None)
 
 }
 case class Some[+A](get: A) extends Option[A]
